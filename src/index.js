b3f0ef3f81d33ef76f4b356719b32a59481287dd
Yash Tiwari <ramjitiwari4455@gmail.com>
2025-05-20 09:58:03 +0000

    Learned about date-fns library

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 import "./styles/style.css";
-import "./styles/style.css";
 import { greeting } from "./modules/greeting.js";
 
 console.log(greeting);
+
+import { format, isToday, isPast, compareAsc } from 'date-fns';
+
+const dueDate = new Date('2025-12-21');
+
+if (isToday(dueDate)) {
+  console.log("Due today!");
+}
+
+if (isPast(dueDate)) {
+  console.log("This task is overdue!");
+}
+
+console.log("Formatted Date:", format(dueDate, 'dd MMM yyyy')); // 25 May 2025
+
+
+import { isBefore, isAfter } from 'date-fns';
+
+const due = new Date('2025-05-21');
+const today = new Date();
+
+console.log(isAfter(due, today)); // true ya false
+
+
+import { differenceInDays } from 'date-fns';
+
+const daysLeft = differenceInDays(new Date('2025-05-25'), new Date());
+console.log(daysLeft);
+
+import { parseISO } from 'date-fns';
+
+const dateObj = parseISO('2025-05-25');
+console.log(dateObj)
